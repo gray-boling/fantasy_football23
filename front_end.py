2f16b7b9be2d0505b1c50abc5b357db05bdcf562
Grayson <grayboling@gmail.com>
2023-09-14 12:45:13 -0600

    first push

---
+++ front_end.py
@@ -0,0 +1,59 @@
+import pandas as pd
+
+from utils import *
+
+stl.title("NFL Fantasy Predictor 2023")
+
+
+here = os.path.dirname(os.path.abspath(__file__))
+
+sched_url = os.path.join(here, 'full_season.csv')
+weekly_url = os.path.join(here, 'weekly_calc_stats.csv')
+
+sched = pd.read_csv(sched_url)
+weekly = pd.read_csv(weekly_url)
+
+gb_pipe = load(os.path.join(here, 'GradientBoostingRegressor.joblib'))
+lin_reg_pipe = load(os.path.join(here, 'LinearRegression.joblib'))
+ran_for_pipe = load(os.path.join(here, 'RandomForestRegressor.joblib'))
+
+infer_df = get_infer_df(sched, weekly)
+
+weekly['week'] = weekly['week'] + 1
+drop_cols = [col for col in sched.columns if 'season' not in col and 'week' not in col]
+weekly = weekly.drop(drop_cols, axis=1)
+weekly = player_sched_join(weekly, infer_df)
+
+infer_df = player_sched_join(weekly, infer_df)
+
+feature_list = ['player_id', 'position', 'week', 'team_year', 'player_year', 'opp_year',
+                'weekday', 'away_team', 'home_team', 'spread_line', 'away_spread_odds', 'home_spread_odds', 'player_is_home',
+                'total_line', 'under_odds', 'over_odds', 'div_game', 'roof'
+                ] + \
+                [col for col in weekly.columns if '_avg' in col] + \
+                [col for col in weekly.columns if '_std' in col] + \
+                [col for col in weekly.columns if '_last' in col] + \
+                [col for col in weekly.columns if '_opp' in col]
+
+
+infer_df['gb_pipe_preds'] = gb_pipe.predict(infer_df[feature_list])
+infer_df['lin_reg_pipe_preds'] = lin_reg_pipe.predict(infer_df[feature_list])
+infer_df['ran_for_pipe_preds'] = ran_for_pipe.predict(infer_df[feature_list])
+
+infer_df['Projected_PPR_Points'] = (infer_df['gb_pipe_preds'] + infer_df['lin_reg_pipe_preds'] + infer_df['ran_for_pipe_preds'])
+infer_df['Projected_PPR_Points'] = (infer_df['Projected_PPR_Points'] / float(3)).round(1)
+infer_df['Lowest_Projected_Points'] = (infer_df['Projected_PPR_Points'] - mean_squared_error(infer_df['fantasy_points_ppr'], infer_df['Projected_PPR_Points'], squared=False)).round(1)
+infer_df['Lowest_Projected_Points'] = np.where(infer_df['Lowest_Projected_Points'] < 0, 0, infer_df['Lowest_Projected_Points'])
+infer_df['Highest_Projected_Points'] = (infer_df['Projected_PPR_Points'] + mean_squared_error(infer_df['fantasy_points_ppr'], infer_df['Projected_PPR_Points'], squared=False)).round(1)
+
+
+user_input_player = stl.text_input("Search team by city/name in the field below")
+if user_input_player:
+    per_team = pd.DataFrame(infer_df[(infer_df['team_full_name'].str.contains(str(user_input_player.title().upper()))) | \
+                                     (infer_df['recent_team'].str.contains(str(user_input_player.title().upper(git))))] \
+                            [['player_name', 'Projected_PPR_Points',	'Lowest_Projected_Points',	'Highest_Projected_Points']])
+    stl.dataframe(per_team)
+else:
+    infer_df.sort_values('Projected_PPR_Points', ascending=False) \
+        [['player_name', 'Projected_PPR_Points', 'Lowest_Projected_Points', 'Highest_Projected_Points']]
+stl.text("")
---
+++ utils.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import numpy as np
+from datetime import date
+from category_encoders import TargetEncoder, CatBoostEncoder
+from sklearn.metrics import mean_squared_error
+from joblib import dump, load
+import streamlit as stl
+import os
+
+def date_getter():
+  today = date.today()
+  today = pd.to_datetime(today, format='%Y-%m-%d').normalize()
+  year = today.strftime("%Y")
+  return (today, int(year))
+
+def player_sched_join(weekly, sched):
+  weekly = weekly \
+  .merge(sched,
+          left_on=["season", 'week',  "recent_team"],
+          right_on=["season", 'week',  "home_team"],
+          how="left",
+          # suffixes=('_weekly', '_sched')
+          ) \
+  .merge(sched,
+          left_on=["season", 'week', "recent_team"],
+          right_on=["season", 'week', "away_team"],
+          how="left",
+          suffixes=('_home_player', '_away_player')
+          )
+  for col in sched.columns:
+    if col not in weekly.columns:
+      weekly[f'{col}_home_player'] = weekly[f'{col}_home_player'].fillna(weekly[f'{col}_away_player'])
+      weekly[f'{col}_away_player'] = weekly[f'{col}_away_player'].fillna(weekly[f'{col}_home_player'])
+  weekly = weekly.rename(columns={c: c.replace('_home_player', '') for c in weekly.columns}) \
+          .rename(columns={c: c.replace('_away_player', '') for c in weekly.columns})
+
+  drop_cols = ['game_id', 'game_type', 'gameday', 'weekday', 'gametime', 'away_team', 'away_score',
+              'home_team', 'home_score', 'location', 'result', 'total', 'overtime', 'old_game_id', 'gsis', 'nfl_detail_id',
+              'pfr', 'pff', 'espn', 'ftn', 'away_rest', 'home_rest', 'away_moneyline', 'home_moneyline', 'spread_line', 'away_spread_odds',
+              'home_spread_odds', 'total_line', 'under_odds', 'over_odds', 'div_game', 'roof', 'surface', 'temp', 'wind',
+              'away_qb_id', 'home_qb_id', 'away_qb_name', 'home_qb_name', 'away_coach', 'home_coach', 'referee', 'stadium_id', 'stadium']
+  # weekly[drop_cols] = weekly[drop_cols].T.drop_duplicates().T
+  weekly = weekly.loc[:,~weekly.columns.duplicated()]
+
+  return weekly
+
+def get_infer_df(sched, weekly):
+  today, year = date_getter()[0], date_getter()[1]
+  sched['gameday'] = pd.to_datetime(sched['gameday'])
+  weekly['gameday'] = pd.to_datetime(weekly['gameday'])
+  if today.strftime("%A") == 'Tuesday':
+      date_setter = sched[sched['gameday'] >= today + pd.Timedelta(2, unit="d")]
+      first_game_of_week = date_setter['gameday'].min().normalize()
+      last_game_of_week = first_game_of_week + pd.Timedelta(4, unit="d")
+  if today.strftime("%A") == 'Wednesday':
+    date_setter = sched[sched['gameday'] >= today + pd.Timedelta(1, unit="d")]
+    first_game_of_week = date_setter['gameday'].min().normalize()
+    last_game_of_week = first_game_of_week + pd.Timedelta(3, unit="d")
+  if today.strftime("%A") == 'Thursday':
+    date_setter = sched[sched['gameday'] >= today]
+    first_game_of_week = date_setter['gameday'].min().normalize()
+    last_game_of_week = first_game_of_week + pd.Timedelta(3, unit="d")
+
+  infer_df = sched[(sched['season'] == year) & ((sched['gameday'] >= first_game_of_week) & (sched['gameday'] <= last_game_of_week))].copy()
+  # infer_df['week'] = infer_df['week'] + 1
+  # infer_df = player_sched_join(infer_df,sched)
+  return infer_df
